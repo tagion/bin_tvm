98097d5d44c0b34dc3f1b14115237437d85ffd2b
cbleser <cr@tagion.org>
2021-06-25 14:06:56 +0300

    WIP: .wasm .so example added

--- tvm.d
+++ tvm.d
@@ -12,6 +12,10 @@ struct Options {
 }
 
 import tagion.basic.Basic : doFront;
+import tagion.tvm.TVM;
+// import tagion.tvm.c.wasm_export;
+// import tagion.tvm.c.wasm_runtime_common;
+
 import std.algorithm.iteration : filter, each;
 import std.path : extension;
 import std.stdio;
@@ -19,12 +23,13 @@ import std.math;
 import std.string : toStringz, fromStringz;
 
 import core.sys.posix.dlfcn;
+import core.stdc.stdio;
 
 //#include <stdio.h>
 //#include "bh_platform.h"
 //#include "wasm_export.h"
 //#include "math.h"
-
+version(none)
 extern(C) {
     import tagion.tvm.c.wasm_export;
     import tagion.tvm.c.wasm_runtime_common;
@@ -115,10 +120,13 @@ enum ext {
     dll = ".so",
 }
 
-extern(C) void fun() {};
+extern(C) {
+    void set_symbols(ref WamrSymbols wasm_symbols);
+    void run(ref TVMEngine wasm_engine);
+    void do_hello();
+}
 
 int main(string[] args) {
-    import tagion.tvm.TVM;
     import std.stdio;
     import std.file : fread=read, exists;
 
@@ -129,42 +137,102 @@ int main(string[] args) {
         .filter!((a) => (a.extension == ext.dll))
         .doFront;
 
-    if (dll_file) {
-        writefln("dll_file=%s", dll_file);
-        void* hndl = dlopen(dll_file.ptr, RTLD_LAZY);
-        if (!hndl) assert(0);
-        pragma(msg, typeof(&fun));
-        auto p = cast(typeof(&fun))dlsym(hndl, fun.mangleof.ptr);
-        pragma(msg, typeof(p));
-        printf("p=%p\n", p);
-        // void function() f_p;
-        p();
-//        f_p=p;
-
+    if (!dll_file) {
+        stderr.writefln("Shared object file missing");
+        return 1;
     }
-    if (wasm_file) {
+
+    if (!wasm_file) {
+        stderr.writefln("WASM file missing");
+        return 2;
+    }
+
+    if (!dll_file.exists) {
+        stderr.writefln("%s does not exists", dll_file);
+        return 3;
+    }
+
+    if (!wasm_file.exists) {
+        stderr.writefln("%s does not exists", wasm_file);
+        return 4;
+    }
+
+    immutable wasm_code = cast(immutable(ubyte[]))wasm_file.fread;
+
+    writefln("wasm_code.length=%d", wasm_code.length);
+
+    void* hndl = dlopen(dll_file.toStringz, RTLD_LAZY);
+    printf("hndl=%p", hndl);
+    scope(exit) {
+        dlclose(hndl);
+    }
+    if (!hndl) {
+        stderr.writefln("Unable to open %s", dll_file);
+        return 5;
+    }
+
+    auto dll_do_hello= cast(typeof(&do_hello))dlsym(hndl, do_hello.mangleof.ptr);
+    if (!dll_do_hello) {
+        printf("%s\n", dlerror());
+        return 6;
+    }
+
+    dll_do_hello();
+
+    auto dll_set_symbols= cast(typeof(&set_symbols))dlsym(hndl, set_symbols.mangleof.ptr);
+    if (!dll_set_symbols) {
+        printf("%s\n", dlerror());
+        return 7;
+    }
+
+    auto dll_run= cast(typeof(&run))dlsym(hndl, run.mangleof.ptr);
+    if (!dll_run) {
+        import core.stdc.stdio;
+        printf("%s\n", dlerror());
+        return 8;
+    }
+
+//     if (dll_file) {
+//         writefln("dll_file=%s", dll_file);
+//         void* hndl = dlopen(dll_file.ptr, RTLD_LAZY);
+//         if (!hndl) assert(0);
+//         pragma(msg, typeof(&fun));
+//         auto p = cast(typeof(&fun))dlsym(hndl, fun.mangleof.ptr);
+//         pragma(msg, typeof(p));
+//         printf("p=%p\n", p);
+//         // void function() f_p;
+//         p();
+// //        f_p=p;
+
+//     }
+
+//     if (wasm_file) {
 //    const testapp_file=args[1];
-        immutable wasm_code = cast(immutable(ubyte[]))wasm_file.fread;
-        WamrSymbols wasm_symbols;
+//        immutable wasm_code = cast(immutable(ubyte[]))wasm_file.fread;
+    WamrSymbols wasm_symbols;
+    dll_set_symbols(wasm_symbols);
         //wasm_symbols("intToStr", &intToStr, "(i*~i)i");
-        wasm_symbols.declare!intToStr; //calculate_native(); //("calculate_native", &calculate_native, "");
-        wasm_symbols.declare!get_pow;
-        wasm_symbols.declare!calculate_native; //("calculate_native", &calculate_native, "");
+        // wasm_symbols.declare!intToStr; //calculate_native(); //("calculate_native", &calculate_native, "");
+        // wasm_symbols.declare!get_pow;
+        // wasm_symbols.declare!calculate_native; //("calculate_native", &calculate_native, "");
 
         // writefln("paramSymbols =%s", WamrSymbols.paramSymbols!calculate_native());
         // writefln("paramSymbols =%s", WamrSymbols.paramSymbols!get_pow());
         // writefln("paramSymbols =%s", WamrSymbols.paramSymbols!intToStr());
-        uint[] global_heap;
-        global_heap.length=512 * 1024;
+    uint[] global_heap;
+    global_heap.length=512 * 1024;
 
-        auto wasm_engine=new TVMEngine(
-            wasm_symbols,
-            8092, // Stack size
-            8092, // Heap size
-            global_heap, // Global heap
-            wasm_code,
-            "env");
+    auto wasm_engine=new TVMEngine(
+        wasm_symbols,
+        8092, // Stack size
+        8092, // Heap size
+        global_heap, // Global heap
+        wasm_code,
+        "env");
 
+    dll_run(wasm_engine);
+
+    /+
         //
         // Calling Wasm functions from D
         //
@@ -193,7 +261,7 @@ int main(string[] args) {
             auto ret=wasm_engine.call!int(calculate, 3);
             assert(ret == 120);
         }
-    }
+        +/
     writeln("Passed");
 
     return 0;
